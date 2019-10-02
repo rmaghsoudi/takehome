e3340aa4e142dd42bb89d5e6cc55bb01e4df5c1b
rmaghsoudi <romy_mag@outlook.com>
2019-10-02 12:27:26 -0400

    added frequency btn

--- src/People/PeoplePage.js
+++ src/People/PeoplePage.js
@@ -1,38 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, {Component} from 'react'
 import PeopleList from './PeopleList';
+import FrequencyList from './FrequencyList';
 
-const PeoplePage = () => {
-  const apiUrl = 'https://api.salesloft.com/v2/people.json'
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-  // code to run on component mount
-  useEffect(() => {
-    fetch(proxyUrl + apiUrl, {
+class PeoplePage extends Component {
+
+  apiUrl = 'https://api.salesloft.com/v2/people.json'
+  // using a proxy due to cors issues
+  proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+
+  state = {
+    people: [],
+    charCount: {},
+    uniqueToggle: false
+  }
+
+  componentDidMount() {
+    fetch(this.proxyUrl + this.apiUrl, {
       method: "GET",
       headers: {
         'Content-Type': 'application/json',
         'Accepts': 'application/json',
         'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`
-      }
-    })
+        }
+      })
       .then(resp => resp.json())
       .then(data => {
-        //TODO: Limit amount of data fetched
-        setPeople(data.data)
+        this.setState({people: data.data})
       })
-  })
-  // Declare a new state variable to contain the data
-  const [people, setPeople] = useState([]);
-  return (
-    <div>
-      <h1>Welcome to the People-Email-Unique-Character-Counter-inator</h1>
-      {/* if people is an array and isn't empty, render the list */}
-      {Array.isArray(people) && people.length ? 
-        <PeopleList people={people}></PeopleList> :
-        <h3>No people here</h3>
     }
-    </div>
-  );
+    
+    uniquinator = (e) => {
+      // displays the frequency count of all the unique chars in the e-mails sorted by frequency count DESC
+      const count = {}
+      this.state.people.map(person => {
+        person.email_address.split('').map(letter => {
+          if (count[letter]) {
+            count[letter] ++
+          } else {
+            count[letter] = 1
+          }
+        })
+      })
+      this.setState({charCount: count, uniqueToggle: !this.state.uniqueToggle})
+    }
+  
+    render() {
+      return (
+        <div>
+        <h1>Welcome to the People-Email-Unique-Character-Counter-inator</h1>
+        {/* if people is an array and isn't empty, render the list */}
+        {Array.isArray(this.state.people) && this.state.people.length ? 
+          <>
+          <button onClick={this.uniquinator}>Toggle Uniquify</button>
+          <PeopleList people={this.state.people}></PeopleList>
+          </> :
+          <h3>No people here</h3>
+        }
+        {this.state.uniqueToggle ?  <FrequencyList charCount={this.state.charCount}></FrequencyList> : null}
+      </div>
+      )
+    }
+
 }
+
 export default PeoplePage
-// what I need to display for each user: name, email address, job title
-// add button that displays the frequency count of all the unique chars in the e-mails sorted by frequency count DESC
